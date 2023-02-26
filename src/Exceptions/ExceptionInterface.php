e1a02c7bffbed55ec6c47c2b5c14a3c1c37bf101
Reactmore <reactmorecom@gmail.com>
2023-02-27 02:24:37 +0700

    initial

---
+++ src/Exceptions/ExceptionInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ReactMoreTech\Support\Exceptions;
+
+/**
+ * Provides a domain-level interface for broad capture
+ * of all framework-related exceptions.
+ *
+ * catch (\CodeIgniter\Exceptions\ExceptionInterface) { ... }
+ */
+interface ExceptionInterface
+{
+}
---
+++ src/PeriodTime.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace ReactMoreTech\Support;
+
+use CodeIgniter\I18n\Time;
+use ReactMoreTech\Support\Exceptions\Exception;
+use DateTimeInterface;
+
+class PeriodTime
+{
+    public function __construct(DateTimeInterface $startDate, DateTimeInterface $endDate)
+    {
+        if ($startDate > $endDate) {
+            throw Exception::startDateCannotBeAfterEndDate($startDate, $endDate);
+        }
+
+        $this->startDate = $startDate;
+
+        $this->endDate = $endDate;
+    }
+
+    public DateTimeInterface $startDate;
+
+    public DateTimeInterface $endDate;
+
+    public static function create(DateTimeInterface $startDate, DateTimeInterface $endDate): self
+    {
+        return new static($startDate, $endDate);
+    }
+
+    public static function days(int $numberOfDays): static
+    {
+        $endDate = Time::today()->toDateTime();
+
+        $startDate = Time::today()->subDays($numberOfDays)->toDateTime();
+
+        return new static($startDate, $endDate);
+    }
+
+    public static function months(int $numberOfMonths): static
+    {
+        $endDate = Time::today()->toDateTime();
+
+        $startDate = Time::today()->subMonths($numberOfMonths)->toDateTime();
+
+        return new static($startDate, $endDate);
+    }
+
+    public static function years(int $numberOfYears): static
+    {
+        $endDate = Time::today()->toDateTime();
+
+        $startDate = Time::today()->subYears($numberOfYears)->toDateTime();
+
+        return new static($startDate, $endDate);
+    }
+}
